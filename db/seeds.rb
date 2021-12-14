38dc9acd2225e29b5fd21d33fc724ed8027e834f
Chris Hewitt <agop5134@gmail.com>
2021-12-14 14:40:14 -0700

    Begin challenge 2:40 pm

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 universal = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
 
 universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
-universal.movies.create!(title: 'Shrek', creation_year: 2001, genre: 'Comedy')
+universal.movies.create!(title: 'Shrek', creation_year: 2001, genre: 'Horror')
