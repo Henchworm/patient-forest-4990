c0ef3d785dea7166fb779a0a4350c82bc3e59aae
Chris Hewitt <agop5134@gmail.com>
2021-12-14 15:55:22 -0700

    Create movie instance methods average_age & youngest_first

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,12 @@ class Movie < ApplicationRecord
   belongs_to :studio
   has_many :movie_actors
   has_many :actors, through: :movie_actors
+
+  def youngest_first
+    self.actors.order(:age)
+  end
+
+  def average_age
+    self.actors.average(:age).to_s
+  end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -6,4 +6,22 @@ RSpec.describe Movie, type: :model do
     it { should have_many(:movie_actors) }
     it { should have_many(:actors).through(:movie_actors) }
   end
+
+  describe "instance methods" do
+    it "orders actors by youngest to oldest" do
+      universal = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
+      raiders = universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
+      harrison = raiders.actors.create!(name: 'Harrison Ford', age: 100 )
+      karen = raiders.actors.create!(name:'Karen Allen', age: 50)
+      expect(raiders.youngest_first).to eq([karen, harrison])
+    end
+
+    it "finds the average age of all actors" do
+      universal = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
+      raiders = universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
+      harrison = raiders.actors.create!(name: 'Harrison Ford', age: 100 )
+      karen = raiders.actors.create!(name:'Karen Allen', age: 50)
+      expect(raiders.average_age).to eq('75.0')
+    end
+  end
 end
